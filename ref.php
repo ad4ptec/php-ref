7eb8c2873bc13152caf6d4a48da159e3ea46195d
ryphoe <ad4ptec@gmail.com>
2020-11-10 22:02:19 +0400

    Update ref.php
    PHP 7 >= 7.1.0

--- ref.php
+++ ref.php
@@ -469,7 +469,7 @@ class ref{
       // expecting 3 components on the 'param' tag: type varName varDescription
       if($tag === 'param'){
         $lastIdx = 2;
-        if(in_array($parts[1][0], array('&', '$'), true)){
+        if(isset($parts[1][0]) && in_array($parts[1][0], array('&', '$'), true)){
           $line     = ltrim(array_pop($parts));
           $parts    = array_merge($parts, explode(' ', $line, 2));        
           $parts[2] = isset($parts[2]) ? ltrim($parts[2]) : null;
@@ -2017,7 +2017,7 @@ class ref{
             $hasType = static::$env['is7'] && $parameter->hasType();
             if($hasType){
               $type = $parameter->getType();
-              $this->fmt->text('hint', (string)$type);
+              $this->fmt->text('hint', $type->getName());
               $this->fmt->sep(' ');
             }
           }
@@ -2057,7 +2057,7 @@ class ref{
           $type = $method->getReturnType();
           $this->fmt->startContain('ret');
           $this->fmt->sep(':');
-          $this->fmt->text('hint', (string)$type);
+          $this->fmt->text('hint', $type->getName());
           $this->fmt->endContain();
         }
 
